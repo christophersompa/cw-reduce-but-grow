6bf619dc16b2d568839b42c609db14f2e2c25812
Christopher <cgtsompa@gmail.com>
2024-10-13 22:43:43 +0100

    Add project files.

---
+++ cw-reduce-but-grow/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw_reduce_but_grow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* 
+             * Given a non-empty array of integers, return the result of multiplying
+             * the values together in or order e.g. [1,2,3,4] = 1 * 2 * 3 * 4 = 24
+             */
+        }
+    }
+}
