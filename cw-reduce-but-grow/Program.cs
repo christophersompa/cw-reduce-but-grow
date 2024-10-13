c87e8054de363fe67c286b05ec55090df6252d0a
Christopher <cgtsompa@gmail.com>
2024-10-13 23:17:55 +0100

    done

--- cw-reduce-but-grow/Program.cs
+++ cw-reduce-but-grow/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,22 @@ namespace cw_reduce_but_grow
              * Given a non-empty array of integers, return the result of multiplying
              * the values together in or order e.g. [1,2,3,4] = 1 * 2 * 3 * 4 = 24
              */
+
+            int[] nums = { 1, 2, 3, 4 };
+
+            Console.WriteLine(Grow(nums));
+
+            Console.ReadLine();
+        }
+
+        public static int Grow(int[] x)
+        {
+            int sum = 1;
+            for (int i = 0; i < x.Length; i++)
+            {
+                sum *= x[i];
+            }
+            return sum;
         }
     }
 }
